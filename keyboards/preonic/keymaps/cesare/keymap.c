f126e710a6f3fb3484f34439c762262d4cfc35a2
Cesare Tagliaferri <cesare@tsc.ai>
2019-02-19 10:42:33 +0800

    Added some macros

--- keyboards/preonic/keymaps/cesare/keymap.c
+++ keyboards/preonic/keymaps/cesare/keymap.c
@@ -32,7 +32,7 @@ enum preonic_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_preonic_grid( \
-  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,\
+  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9, KC_MS_BTN2, KC_BSPC,\
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,\
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,\

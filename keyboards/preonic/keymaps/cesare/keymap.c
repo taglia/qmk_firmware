e28590c172456dc67d07176342c0b0974109f1ef
Cesare Tagliaferri <cesare@tsc.ai>
2019-03-07 18:54:57 +0800

    Added OmniFocus macro

--- keyboards/preonic/keymaps/cesare/keymap.c
+++ keyboards/preonic/keymaps/cesare/keymap.c
@@ -18,7 +18,8 @@ enum preonic_keycodes {
   SCR_LOCK,
   EJCT_TM,
   NXT_TAB,
-  PRV_TAB
+  PRV_TAB,
+  OF_VIEW
 };
 
 // My macros (code below)
@@ -32,7 +33,7 @@ enum preonic_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_preonic_grid( \
-  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9, KC_MS_BTN2, KC_BSPC,\
+  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9, KC_MS_BTN2, OF_VIEW,\
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,\
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,\
@@ -114,7 +115,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING("0");
             wait_ms(200);
             SEND_STRING("0");
-            wait_ms(200);
+          } else {
             SEND_STRING(SS_UP(X_LCTRL)SS_UP(X_LALT)SS_UP(X_LGUI)SS_UP(X_LSHIFT));
           }
           return FALSE;
@@ -123,7 +124,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LSHIFT));
             wait_ms(200);
             SEND_STRING(SS_DOWN(X_MEDIA_EJECT));
-            wait_ms(200);
+          } else {
             SEND_STRING(SS_UP(X_MEDIA_EJECT));
             wait_ms(200);
             SEND_STRING(SS_UP(X_LCTRL)SS_UP(X_LSHIFT));
@@ -141,6 +142,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LSHIFT)SS_DOWN(X_TAB));
           } else {
             SEND_STRING(SS_TAP(X_TAB)SS_UP(X_LCTRL)SS_UP(X_LSHIFT));
+          }
+          return FALSE;
+        case OF_VIEW:
+          if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI)SS_DOWN(X_LALT)SS_DOWN(X_LCTRL)SS_DOWN(X_LSHIFT));
+            wait_ms(200);
+            SEND_STRING(SS_DOWN(X_T));
+          } else {
+            SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTRL)SS_UP(X_LSHIFT)SS_UP(X_T));
           }
           return FALSE;
       }

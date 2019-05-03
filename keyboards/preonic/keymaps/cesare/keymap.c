77db129b550d34dd1b3896f652a7b9c0467c410d
Cesare Tagliaferri <taglia@me.com>
2019-05-03 18:19:48 +0800

    Removed cursor keys from the bottom right on RAISE and LOWER

--- keyboards/preonic/keymaps/cesare/keymap.c
+++ keyboards/preonic/keymaps/cesare/keymap.c
@@ -5,6 +5,7 @@
 
 enum preonic_layers {
   _QWERTY,
+  _COLEMAK,
   _LOWER,
   _RAISE,
   _ADJUST
@@ -12,6 +13,7 @@ enum preonic_layers {
 
 enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
+  COLEMAK,
   LOWER,
   RAISE,
   BACKLIT,
@@ -36,12 +38,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC, KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_LCTL,  KC_SPC,  RAISE,    KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT\
 ),
 
+[_COLEMAK] = LAYOUT_preonic_grid( \
+  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   SSHOT,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    GUI_W, OF_VIEW,\
+  KC_GRV,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,  \
+  KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
+  KC_ESC,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_LCTL,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT  \
+),
+
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    OF_REVIEW, \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RPRN, KC_DEL,\
   KC_CAPS, KC_MUTE, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, KC_PLUS, KC_MINS, KC_ASTR, KC_LCBR, KC_RCBR, KC_BSLS,\
   KC_LSFT, KC_MPLY,  KC_MRWD, KC_MPLY, KC_MFFD, XXXXXXX, KC_EQL,  KC_UNDS, XXXXXXX, KC_LBRC, KC_RBRC, KC_ENT,\
-  KC_ESC, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______,  KC_DOWN, KC_UP,   KC_RGUI, KC_RSFT\
+  KC_ESC, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______,  _______,  _______, _______, _______\
   ),
 
 [_LOWER] = LAYOUT_preonic_grid( \
@@ -49,14 +59,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,\
   KC_TAB,   KC_DEL,  KC_INS, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, XXXXXXX, KC_PIPE,\
   KC_LSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,   KC_END, XXXXXXX, KC_ENT,\
-  KC_ESC, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______,  KC_DOWN,  KC_UP,   KC_RGUI, KC_RSFT\
+  KC_ESC, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______,  _______,  _______, _______, _______\
 ),
 
 [_ADJUST] = LAYOUT_preonic_grid( \
   DEBUG,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EJCT_TM,  \
   RESET,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_PAUSE ,\
   KC_TAB,   KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20, KC_PSCREEN,\
-  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  QWERTY, XXXXXXX, XXXXXXX, BACKLIT, XXXXXXX,\
+  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  QWERTY, COLEMAK, XXXXXXX, BACKLIT, XXXXXXX,\
   KC_SLCK, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX\
 )
 
@@ -70,6 +80,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             set_single_persistent_default_layer(_QWERTY);
           }
           return FALSE;
+        case COLEMAK:
+          if (record->event.pressed) {
+            set_single_persistent_default_layer(_COLEMAK);
+          }
+          return false;
+          break;
         case LOWER:
           if (record->event.pressed) {
             layer_on(_LOWER);

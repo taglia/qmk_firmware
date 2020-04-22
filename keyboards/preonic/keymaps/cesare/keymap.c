df62fc2f5ef4d3f587bd2406c1ad547f17767b1c
Cesare Tagliaferri <taglia@me.com>
2020-04-22 18:00:00 +0800

    Added some macros

--- keyboards/preonic/keymaps/cesare/keymap.c
+++ keyboards/preonic/keymaps/cesare/keymap.c
@@ -25,13 +25,16 @@ enum preonic_keycodes {
   OF_REVIEW,
   OF_INBOX,
   GUI_W,
-  SSHOT
+  SSHOT,
+  SPOTLIGHT,
+  WEB_SEARCH,
+  WINDOW_MANIPULATION
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_preonic_grid( \
-  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   SSHOT,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    GUI_W, OF_VIEW,\
+  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   SSHOT,   WINDOW_MANIPULATION,   WEB_SEARCH,    SPOTLIGHT,    KC_8,    KC_9,    GUI_W, OF_VIEW,\
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,\
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,\
@@ -39,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_COLEMAK] = LAYOUT_preonic_grid( \
-  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   SSHOT,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    GUI_W, OF_VIEW,\
+  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   SSHOT,   WINDOW_MANIPULATION,   WEB_SEARCH,   SPOTLIGHT,    KC_8,    KC_9,    GUI_W, OF_VIEW,\
   KC_GRV,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,  \
   KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
@@ -197,6 +200,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING(SS_DOWN(X_4));
           } else {
             SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LSHIFT)SS_UP(X_4));
+          }
+          return FALSE;
+        case SPOTLIGHT:
+          if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI)SS_DOWN(X_SPACE));
+          } else {
+            SEND_STRING(SS_UP(X_SPACE)SS_UP(X_LGUI));
+          }
+          return FALSE;
+        case WEB_SEARCH:
+          if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI)SS_DOWN(X_LALT)SS_DOWN(X_LCTRL)SS_DOWN(X_L));
+          } else {
+            SEND_STRING(SS_UP(X_L)SS_UP(X_LCTRL)SS_UP(X_LALT)SS_UP(X_LGUI));
+          }
+          return FALSE;
+        case WINDOW_MANIPULATION:
+          if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI)SS_DOWN(X_LALT)SS_DOWN(X_LCTRL)SS_DOWN(X_W));
+          } else {
+            SEND_STRING(SS_UP(X_W)SS_UP(X_LCTRL)SS_UP(X_LALT)SS_UP(X_LGUI));
           }
           return FALSE;
       }

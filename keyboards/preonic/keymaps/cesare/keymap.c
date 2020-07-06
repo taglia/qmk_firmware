a096a00eefc2e89b46de5b478d802e261938d5f0
Cesare Tagliaferri <taglia@me.com>
2020-07-06 21:16:27 +0800

    Added macros

--- keyboards/preonic/keymaps/cesare/keymap.c
+++ keyboards/preonic/keymaps/cesare/keymap.c
@@ -28,13 +28,14 @@ enum preonic_keycodes {
   SSHOT,
   SPOTLIGHT,
   WEB_SEARCH,
-  WINDOW_MANIPULATION
+  WINDOW_MANIPULATION,
+  MAIL_ARCHIVE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_preonic_grid( \
-  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   SSHOT,   WINDOW_MANIPULATION,   WEB_SEARCH,    SPOTLIGHT,    KC_8,    KC_9,    GUI_W, OF_VIEW,\
+  SCR_LOCK, PRV_TAB, NXT_TAB, GUI_W,   SSHOT,   WINDOW_MANIPULATION,   WEB_SEARCH,    SPOTLIGHT,    KC_8,  GUI_W, MAIL_ARCHIVE, OF_VIEW,\
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,\
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,\
@@ -42,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_COLEMAK] = LAYOUT_preonic_grid( \
-  SCR_LOCK, PRV_TAB, NXT_TAB, KC_3,   SSHOT,   WINDOW_MANIPULATION,   WEB_SEARCH,   SPOTLIGHT,    KC_8,    KC_9,    GUI_W, OF_VIEW,\
+  SCR_LOCK, PRV_TAB, NXT_TAB, GUI_W,   SSHOT,   WINDOW_MANIPULATION,   WEB_SEARCH,   SPOTLIGHT,    KC_8,   GUI_W, MAIL_ARCHIVE, OF_VIEW,\
   KC_GRV,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,  \
   KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
@@ -221,6 +222,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING(SS_DOWN(X_LGUI)SS_DOWN(X_LALT)SS_DOWN(X_LCTRL)SS_DOWN(X_W));
           } else {
             SEND_STRING(SS_UP(X_W)SS_UP(X_LCTRL)SS_UP(X_LALT)SS_UP(X_LGUI));
+          }
+          return FALSE;
+        case MAIL_ARCHIVE:
+          if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI)SS_DOWN(X_LCTRL)SS_DOWN(X_A));
+          } else {
+            SEND_STRING(SS_UP(X_A)SS_UP(X_LCTRL)SS_UP(X_LGUI));
           }
           return FALSE;
       }
